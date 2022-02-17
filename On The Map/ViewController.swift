ca8ccd426c842f671f3f77979a93d74179cfaba7
Solomon Dove <solomondove@mgmail.com>
2022-02-16 17:36:53 -0800

    Initial Commit

---
+++ On The Map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by Solomon Dove on 2/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
